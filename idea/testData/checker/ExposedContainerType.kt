a9a4359bc52a0eb8a3c1206f8002a098b0e7076d
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-02-12 18:32:31 +0300

    "Exposed" diagnostic messages were simplified

--- idea/testData/checker/ExposedContainerType.kt
+++ idea/testData/checker/ExposedContainerType.kt
@@ -8,13 +8,13 @@ class A {
     }
 }
 
-fun <error descr="[EXPOSED_RECEIVER_TYPE] Member effective visibility 'public' should be the same or less permissive than its receiver type containing declaration Companion effective visibility 'internal'">A.Companion.B.C</error>.foo() {}
+fun <error descr="[EXPOSED_RECEIVER_TYPE] 'public' member exposes its 'internal' receiver type containing declaration Companion">A.Companion.B.C</error>.foo() {}
 
-interface E : <error descr="[EXPOSED_SUPER_INTERFACE] Sub-interface effective visibility 'public' should be the same or less permissive than its supertype containing declaration Companion effective visibility 'internal'">A.Companion.B.D</error>
+interface E : <error descr="[EXPOSED_SUPER_INTERFACE] 'public' sub-interface exposes its 'internal' supertype containing declaration Companion">A.Companion.B.D</error>
 
-val <error descr="[EXPOSED_PROPERTY_TYPE] Property effective visibility 'public' should be the same or less permissive than its type containing declaration Companion effective visibility 'internal'">x</error> = A.Companion.B
+val <error descr="[EXPOSED_PROPERTY_TYPE] 'public' property exposes its 'internal' type containing declaration Companion">x</error> = A.Companion.B
 
-class F<<error descr="[EXPOSED_TYPE_PARAMETER_BOUND] Generic effective visibility 'public' should be the same or less permissive than its parameter bound type containing declaration Companion effective visibility 'internal'">T : A.Companion.B</error>>(val x: T)
+class F<<error descr="[EXPOSED_TYPE_PARAMETER_BOUND] 'public' generic exposes its 'internal' parameter bound type containing declaration Companion">T : A.Companion.B</error>>(val x: T)
 
 
 
--- idea/testData/checker/ExposedInferredType.kt
+++ idea/testData/checker/ExposedInferredType.kt
@@ -2,18 +2,18 @@ private class A
 
 open class B<T>(val x: T)
 
-class C(<error descr="[EXPOSED_PARAMETER_TYPE] Function effective visibility 'public' should be the same or less permissive than its parameter type A effective visibility 'private'">x: A</error>): <error descr="[EXPOSED_SUPER_CLASS] Subclass effective visibility 'public' should be the same or less permissive than its supertype argument A effective visibility 'private'">B<A>(x)</error>
+class C(<error descr="[EXPOSED_PARAMETER_TYPE] 'public' function exposes its 'private' parameter type A">x: A</error>): <error descr="[EXPOSED_SUPER_CLASS] 'public' subclass exposes its 'private' supertype argument A">B<A>(x)</error>
 
 private class D {
     class E
 }
 
-fun <error descr="[EXPOSED_FUNCTION_RETURN_TYPE] Function effective visibility 'public' should be the same or less permissive than its return type A effective visibility 'private'">create</error>() = A()
+fun <error descr="[EXPOSED_FUNCTION_RETURN_TYPE] 'public' function exposes its 'private' return type A">create</error>() = A()
 
-fun <error descr="[EXPOSED_FUNCTION_RETURN_TYPE] Function effective visibility 'public' should be the same or less permissive than its return type argument A effective visibility 'private'">create</error>(<error descr="[EXPOSED_PARAMETER_TYPE] Function effective visibility 'public' should be the same or less permissive than its parameter type A effective visibility 'private'">a: A</error>) = B(a)
+fun <error descr="[EXPOSED_FUNCTION_RETURN_TYPE] 'public' function exposes its 'private' return type argument A">create</error>(<error descr="[EXPOSED_PARAMETER_TYPE] 'public' function exposes its 'private' parameter type A">a: A</error>) = B(a)
 
-val <error descr="[EXPOSED_PROPERTY_TYPE] Property effective visibility 'public' should be the same or less permissive than its type A effective visibility 'private'">x</error> = create()
+val <error descr="[EXPOSED_PROPERTY_TYPE] 'public' property exposes its 'private' type A">x</error> = create()
 
-val <error descr="[EXPOSED_PROPERTY_TYPE] Property effective visibility 'public' should be the same or less permissive than its type argument A effective visibility 'private'">y</error> = create(x)
+val <error descr="[EXPOSED_PROPERTY_TYPE] 'public' property exposes its 'private' type argument A">y</error> = create(x)
 
-val <error descr="[EXPOSED_PROPERTY_TYPE] Property effective visibility 'public' should be the same or less permissive than its type argument containing declaration D effective visibility 'private'">z</error>: B<D.E>? = null
+val <error descr="[EXPOSED_PROPERTY_TYPE] 'public' property exposes its 'private' type argument containing declaration D">z</error>: B<D.E>? = null
