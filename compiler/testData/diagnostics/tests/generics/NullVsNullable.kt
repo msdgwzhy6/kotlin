8118da95f15e3e340273c4185fb965b7f7625457
Mikhail Glukhikh <mikhail.glukhikh@gmail.com>
2016-02-13 20:55:56 +0300

    ++

--- compiler/testData/diagnostics/tests/generics/NullVsNullable.kt
+++ compiler/testData/diagnostics/tests/generics/NullVsNullable.kt
@@ -10,6 +10,8 @@ class Consume<T>(val x: T, val f: (T) -> Unit)
 
 class Transform<T>(val x: T, val f: (T) -> T)
 
+class Mapper<T, R>(val x: T, val f: (T) -> R)
+
 class Produce<T>(val x: T, val f: () -> T)
 
 val c: Consume<Int?> = Consume(null) { y -> foo(y) }
@@ -19,6 +21,10 @@ val cc = Consume(null) { y -> foo(<!DEBUG_INFO_CONSTANT!>y<!>) }
 val t: Transform<Int?> = <!TYPE_INFERENCE_CONFLICTING_SUBSTITUTIONS!>Transform<!>(null) { y -> bar(<!DEBUG_INFO_CONSTANT!>y<!>) }
 
 val tt = <!TYPE_INFERENCE_CONFLICTING_SUBSTITUTIONS!>Transform<!>(null) { y -> bar(<!DEBUG_INFO_CONSTANT!>y<!>) }
+
+val m: Mapper<Int?, String> = <!TYPE_INFERENCE_EXPECTED_TYPE_MISMATCH!>Mapper(null) { y -> y.toString() }<!>
+
+val mm = Mapper(null) { y -> y.toString() }
 
 val p: Produce<Int?> = Produce(null) { 42 }
 
