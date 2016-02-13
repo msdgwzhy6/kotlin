6e498ef08f848af94bbaf19eb92e72f9539906c2
Mikhail Glukhikh <mikhail.glukhikh@gmail.com>
2016-02-13 11:59:41 +0300

    test extended

--- compiler/testData/diagnostics/tests/generics/NullVsNullable.kt
+++ compiler/testData/diagnostics/tests/generics/NullVsNullable.kt
@@ -3,8 +3,23 @@
 fun foo(arg: Any?) {
     arg?.hashCode()
 }
-class Sample<T>(val x: T, val f: (T) -> Unit)
-val ok:   Sample<Int?> = Sample(null) { y -> } 
 
-// y should be Int? and not Nothing? here
-val fail: Sample<Int?> = Sample(null) { y -> foo(y) }
+fun bar(arg: Any?): Any? = arg?.hashCode()
+
+class Consume<T>(val x: T, val f: (T) -> Unit)
+
+class Transform<T>(val x: T, val f: (T) -> T)
+
+class Produce<T>(val x: T, val f: () -> T)
+
+val c: Consume<Int?> = Consume(null) { y -> foo(y) }
+
+val cc = Consume(null) { y -> foo(<!DEBUG_INFO_CONSTANT!>y<!>) }
+
+val t: Transform<Int?> = <!TYPE_INFERENCE_CONFLICTING_SUBSTITUTIONS!>Transform<!>(null) { y -> bar(<!DEBUG_INFO_CONSTANT!>y<!>) }
+
+val tt = <!TYPE_INFERENCE_CONFLICTING_SUBSTITUTIONS!>Transform<!>(null) { y -> bar(<!DEBUG_INFO_CONSTANT!>y<!>) }
+
+val p: Produce<Int?> = Produce(null) { 42 }
+
+val pp = Produce(null) { 42 }
